230f64b18abaea299584b24a6b574317821ae6d5
Dia <102209509+diana-tuduran@users.noreply.github.com>
2023-10-31 16:28:45 +0200

    Updated register function for RegisterPage.js

--- RegisterPage.js
+++ RegisterPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Image, KeyboardAvoidingView, Alert } from 'react-native';
 
 const RegisterPage = ({ navigation }) => {
   const [name, setName] = useState('');
@@ -7,32 +7,40 @@ const RegisterPage = ({ navigation }) => {
   const [password, setPassword] = useState('');
 
   const handleRegister = () => {
-    // Implement user registration logic here.
-    // After successful registration, you can navigate to the "Login" page.
-    navigation.navigate('Login');
+    const emailPattern = /^[A-Z0-9._%+-]+@domain\.com$/i;
+
+    if (name.length >= 3 && !/\s/.test(name) && email.match(emailPattern) && password.length >= 6) {
+      // Successful registration, navigate to the "Login" page.
+      Alert.alert('Registration Successful', 'You can now log in.');
+      navigation.navigate('Login');
+    } else {
+      // Display an error message for incomplete or invalid registration data.
+      Alert.alert('Registration Failed', 'Please fill in all registration fields correctly.');
+    }
   };
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Image source={require('./path_to_your_image.png')} style={styles.logo} />
       <Text style={styles.heading}>Register</Text>
       <TextInput
         style={styles.input}
-        placeholder="Name"
+        placeholder="Name (at least 3 characters, no spaces)"
         onChangeText={text => setName(text)}
       />
       <TextInput
         style={styles.input}
-        placeholder="Email"
+        placeholder="Email (e.g., user@domain.com)"
         onChangeText={text => setEmail(text)}
       />
       <TextInput
         style={styles.input}
-        placeholder="Password"
+        placeholder="Password (at least 6 characters)"
         secureTextEntry={true}
         onChangeText={text => setPassword(text)}
       />
       <Button title="Register" onPress={handleRegister} />
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -41,15 +49,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
   },
   heading: {
     fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20,
   },
   input: {
-    width: '80%',
+    width: '100%',
     height: 40,
-    borderColor: 'gray',
+    borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 10,
